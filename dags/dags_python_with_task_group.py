168ec727fa7a99e138595f26025749d20a09ed08
Nicole-Bang <bubble4250@gmail.com>
2024-04-12 14:49:28 +0900

    수업내용 추가

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -6,6 +6,10 @@ from airflow.decorators import task
 from airflow.decorators import task_group
 from airflow.utils.task_group import TaskGroup
 
+# Task들의 모임
+# UI Graph탭에서 Task들을 Group화하여 보여줌
+# Task Group 안에 Task Group을 중첩하여 구성 가능
+
 with DAG(
     dag_id="dags_python_with_task_group",
     schedule=None,
@@ -16,9 +20,10 @@ with DAG(
         msg = kwargs.get('msg') or '' 
         print(msg)
 
+    # decorator를 이용하여 만들기 : from airflow.decorators import task_group
     @task_group(group_id='first_group')
     def group_1():
-        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''
+        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''      #docstring :  함수에 대한 설명
 
         @task(task_id='inner_function1')
         def inner_func1(**kwargs):
@@ -32,9 +37,10 @@ with DAG(
 
         inner_func1() >> inner_function2
 
-    with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다') as group_2:
+    # class를 이용하기 : from airflow.utils.task_group import TaskGroup
+    with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다') as group_2:   #tooltip = docstring : 함수를 설명
         ''' 여기에 적은 docstring은 표시되지 않습니다'''
-        @task(task_id='inner_function1')
+        @task(task_id='inner_function1')       #group이 분기되어있을땐 task 이름이 같아도 된다
         def inner_func1(**kwargs):
             print('두 번째 TaskGroup 내 첫 번째 task입니다.')
 
