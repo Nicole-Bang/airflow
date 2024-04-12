15219a03724687b2abbd197222bbcdbc5449a86f
Nicole-Bang <bubble4250@gmail.com>
2024-04-12 10:49:44 +0900

    설명추가

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -12,20 +12,23 @@ with DAG(
     
     @task(task_id='python_xcom_push_by_return')
     def xcom_push_result(**kwargs):
-        return 'Success'
+        return 'Success' #return_value에 Success가 들어감
 
 
     @task(task_id='python_xcom_pull_1')
     def xcom_pull_1(**kwargs):
         ti = kwargs['ti']
-        value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return')
+        value1 = ti.xcom_pull(task_ids='python_xcom_push_by_return') #task_ids값만 주는건 return_value를 찾아온다
         print('xcom_pull 메서드로 직접 찾은 리턴 값:' + value1)
 
     @task(task_id='python_xcom_pull_2')
     def xcom_pull_2(status, **kwargs):
-        print('함수 입력값으로 받은 값:' + status)
+        print('함수 입력값으로 받은 값:' + status)  #status : python_xcom_push_by_return의 success가 들어감
 
 
     python_xcom_push_by_return = xcom_push_result()
     xcom_pull_2(python_xcom_push_by_return)
     python_xcom_push_by_return >> xcom_pull_1()
+
+    # xcom_push_result -> xcom_pull_2
+    # xcom_push_result -> xcom_pull_1 이렇게 돈다
