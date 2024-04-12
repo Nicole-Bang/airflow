68cf4e9655e699e4d683eb906bef7e310d82e3b0
Nicole-Bang <bubble4250@gmail.com>
2024-04-12 11:20:54 +0900

    실습 시 내용 추가

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -4,6 +4,9 @@ import datetime
 from airflow.decorators import task
 from airflow.operators.email import EmailOperator
 
+# Email 오퍼레이터는 어떤 파라미터에 Template을 사용할 수 있나요??
+# to, subject, html_comtent, files 
+
 with DAG(
     dag_id="dags_python_email_operator",
     schedule="0 8 1 * *",
@@ -14,12 +17,12 @@ with DAG(
     @task(task_id='something_task')
     def some_logic(**kwargs):
         from random import choice 
-        return choice(['Success','Fail'])
+        return choice(['Success','Fail']) #random 라이브러리의 choice -> 아무값이나 선택되는 것
 
 
     send_email = EmailOperator(
         task_id='send_email',
-        to='hjkim_sun@naver.com',
+        to='ausdl88@naver.com',
         subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} some_logic 처리결과',
         html_content='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 처리 결과는 <br> \
                     {{ti.xcom_pull(task_ids="something_task")}} 했습니다 <br>'
