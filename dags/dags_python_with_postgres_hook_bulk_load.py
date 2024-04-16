c0bed7902741e8cdb642d1213f57b9db122c9496
Nicole-Bang <bubble4250@gmail.com>
2024-04-16 13:54:58 +0900

    설명 추가

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -20,3 +20,14 @@ with DAG(
                    'tbl_nm':'TbCorona19CountStatus_bulk1',
                    'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
     )
+
+    # Bulk_load의 문제점
+    # Load 가능한 Delimiter는 tab으로 고정되어 있음
+    # Header까지 포함해서 업로드 됨
+    # 특수문자로 인해 파싱이 안될 경우 에러 발생
+
+    # 개선방안
+    # Custom Hook을 만들어서 Delimiter 유형을 입력받게 하고
+    # Header 여부를 선택하게끔 하며
+    # 특수문자를 제거하는 로직을 추가 후
+    # sqlalchemy를 이용하여 Load 한다면? 그리고 테이블을 생성하면서 업로드 한다면??
--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -2,21 +2,25 @@ from airflow.hooks.base import BaseHook
 import psycopg2
 import pandas as pd
 
+# import BaseHook을 했을 때, a = BaseHook()으로 객체화하여 사용하는 경우가 일반적이나
+# @classmethod 데코레이션을 사용시 이런 객체화를 사용하지 않아도 된다.
+
 class CustomPostgresHook(BaseHook):
 
     def __init__(self, postgres_conn_id, **kwargs):
         self.postgres_conn_id = postgres_conn_id
 
     def get_conn(self):
-        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
+        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)   #classMethod 유형 -> BaseHook.get_connection(self.postgres_conn_id)로 다이렉트 접근 가능
         self.host = airflow_conn.host
         self.user = airflow_conn.login
         self.password = airflow_conn.password
         self.dbname = airflow_conn.schema
         self.port = airflow_conn.port
 
+        # psycopg2.connect -> db와의 연결 
         self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
-        return self.postgres_conn
+        return self.postgres_conn   # airflow_conn와는 다른것임을 인지해야함
 
     def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
         from sqlalchemy import create_engine
