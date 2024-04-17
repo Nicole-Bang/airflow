c0db3f7bfe6c0d577810df3d73367c1437204a30
Nicole-Bang <bubble4250@gmail.com>
2024-04-17 16:31:02 +0900

    실습 내용 추가

--- dags/dags_time_sensor.py
+++ dags/dags_time_sensor.py
@@ -6,9 +6,10 @@ with DAG(
     dag_id="dags_time_sensor",
     start_date=pendulum.datetime(2023, 5, 1, 0, 0, 0),
     end_date=pendulum.datetime(2023, 5, 1, 1, 0, 0),
-    schedule="*/10 * * * *",
+    schedule="*/10 * * * *",    # 10분마다 1번씩 돈다
     catchup=True,
 ) as dag:
+    # DateTimeSenso : 목표로 하는 시간까지 기다리는 sensor
     sync_sensor = DateTimeSensor(
         task_id="sync_sensor",
         target_time="""{{ macros.datetime.utcnow() + macros.timedelta(minutes=5) }}""",
--- dags/dags_time_sensor_with_async.py
+++ dags/dags_time_sensor_with_async.py
@@ -2,11 +2,13 @@ import pendulum
 from airflow import DAG
 from airflow.sensors.date_time import DateTimeSensorAsync
 
+# DateTimeSensorAsync와 같이 뒤에 Async가 붙은 오퍼레이터는 Deferrable Operator라 부르며 Triggerer에게 작업 완료 수신을 맡기는 오퍼레이터라는 의미
+
 with DAG(
     dag_id="dags_time_sensor_with_async",
     start_date=pendulum.datetime(2023, 5, 1, 0, 0, 0),
     end_date=pendulum.datetime(2023, 5, 1, 1, 0, 0),
-    schedule="*/10 * * * *",
+    schedule="*/10 * * * *",    # 10분에 1번씩
     catchup=True,
 ) as dag:
     sync_sensor = DateTimeSensorAsync(
